701c87bc7ddef8dfdfde95e6600f5087a64f5026
Lane Olson <laneolson@gmail.com>
2015-03-18 09:39:27 -0600

    styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 
-$("nav a").on("click", function() {
-  $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+$("nav a, #nav li a").on("click", function() {
+  $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top+1 }, 500);
   return false;
+});
+
+$("#nav-toggle").on("click", function() {
+  $(this).parent().toggleClass("active");
 });
 
 // init controller
@@ -30,3 +34,25 @@ var scene = new ScrollMagic.Scene({
               })
               .setTween(contentTween)
               .addTo(controller); // assign the scene to the controller
+
+
+var navigation = new ScrollMagic.Scene({ 
+                offset: $("#music").position().top,
+                duration: $("#music").outerHeight()
+              })
+              .setClassToggle("#nav","fixed-music")
+              .addTo(controller);
+
+var navigation2 = new ScrollMagic.Scene({ 
+                offset: $("#bio").position().top,
+                duration: $("#bio").outerHeight()
+              })
+              .setClassToggle("#nav","fixed-bio")
+              .addTo(controller);
+
+var navigation2 = new ScrollMagic.Scene({ 
+                offset: $("#merch").position().top,
+                duration: $("#merch").outerHeight()
+              })
+              .setClassToggle("#nav","fixed-merch")
+              .addTo(controller);
